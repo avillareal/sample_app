2f253a976b46f8654dd0564d15d28f28951bb195
avillareal <avillareal@sbrforum.com>
2014-03-26 08:36:59 -0600

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-  def show
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+def show
     @user = User.find(params[:id])
   end
 
@@ -7,7 +14,7 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
-  def create
+def create
     @user = User.new(user_params)
     if @user.save
       sign_in @user
@@ -18,10 +25,36 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
